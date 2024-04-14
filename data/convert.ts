c0756eee9de93dac3dd0f8914ff3fdab79d10286
Kisaragi Hiu <mail@kisaragi-hiu.com>
2024-04-14 16:50:15 +0900

    changes to try out zim

--- data/convert.ts
+++ data/convert.ts
@@ -1,12 +1,8 @@
-import { Database } from "bun:sqlite";
 import * as fs from "node:fs";
+import path from "node:path";
 import readline from "node:readline";
-import type { Readable } from "node:stream";
-import { TextDecoderStream } from "@stardazed/streams-text-encoding";
-import { ConcatenatedJsonParseStream } from "@std/json";
-import { pathToFileURL } from "bun";
-import { default as flow } from "xml-flow";
-import type { RawPage } from "./types.ts";
+import { Archive } from "@openzim/libzim";
+import Database from "better-sqlite3";
 
 function progress(
   i: number,
@@ -37,64 +33,46 @@ function progress(
 
 const variant = process.argv[2] || "zh_min_nanwiktionary";
 
-fs.rmSync(`${variant}-articles.sqlite`, { force: true });
-const db = new Database(`${variant}-articles.sqlite`);
-db.run(`
+fs.rmSync(`${variant}-zim-articles.sqlite`, { force: true });
+const db = new Database(`${variant}-zim-articles.sqlite`);
+db.exec(`
 CREATE TABLE pages (
-  id INTEGER PRIMARY KEY,
+  --id INTEGER PRIMARY KEY,
+  path TEXT,
   title TEXT,
-  text TEXT,
-  categories TEXT,
-  redirect TEXT,
-  revisionId INTEGER,
-  lastModified TEXT,
-  lastContributor TEXT
+  data BLOB,
+  redirect TEXT
 )`);
 
-const xmlFile = `${variant}-latest-pages-articles.xml`;
-const xmlStream = flow(fs.createReadStream(xmlFile)) as Readable;
-const htmlFile = `${variant}-htmldump.ndjson`;
-const htmlData = (await fetch(pathToFileURL(htmlFile)))
-  .body as ReadableStream<Uint8Array>;
-const htmlStream = htmlData
-  .pipeThrough(new TextDecoderStream("utf-8", {}))
-  .pipeThrough(new ConcatenatedJsonParseStream());
-
-console.log(`Inserting ${xmlFile} into database...`);
+const zimFile = "wiktionary_ja_all_maxi_2024-04.zim";
+const archive = new Archive(zimFile);
+console.log(`Inserting ${zimFile} into database...`);
 
 const insert = db.prepare(
-  "INSERT INTO pages (id,title,text,redirect,revisionId,lastModified,lastContributor) VALUES ($id,$title,$text,$redirect,$revisionId,$lastModified,$lastContributor)",
+  "INSERT INTO pages (path,title,data,redirect) VALUES ($path,$title,$data,$redirect)",
 );
-console.log("Inserting data from XML dump...");
+console.log("Inserting data from .zim file...");
 let i = 0;
 let lastTime: { time: Date; i: number; diff: number } | undefined;
-db.run("BEGIN TRANSACTION;");
-xmlStream.on("tag:page", (page: RawPage) => {
+db.exec("BEGIN TRANSACTION;");
+for (const entry of archive.iterByPath()) {
   i++;
-  // pages, categories, appendix, thesaurus
-  if (!["0", "14", "100", "110"].includes(page.ns)) return;
-
-  const revision = page.revision;
-  let text: string | undefined;
-  // The HTML dump does not include appendix and thesaurus
-  // There's also a few entries that aren't in the dump for some reason
-  if (typeof revision.text === "object") {
-    text = revision.text.$text;
-  } else {
-    text = revision.text;
-  }
-
+  // JS, CSS, etc.; Images; Zim metadata; Search index
+  if (["-", "I", "M", "X"].includes(entry.path[0])) continue;
+  if (i <= 2000) continue;
+  if (!entry.title.endsWith("用語集")) continue;
+  console.log(`i: ${i}\npath: ${entry.path}\ndata:`);
+  console.log(entry.item.data.toString());
+  process.exit(0);
+  const isRedirect = entry.isRedirect;
+  const item = entry.getItem(true);
   const obj = {
-    $id: Number.parseInt(page.id),
-    $title: page.title,
+    $path: entry.path,
+    $title: entry.title,
     // If it's already a redirect, the text content is redundant
-    $text: text && !page.redirect ? text : null,
-    $redirect: page.redirect || null,
-    $revisionId: Number.parseInt(revision.id),
-    $lastModified: revision.timestamp,
-    $lastContributor: revision.contributor.username,
+    $data: isRedirect ? null : item.data.data,
+    $redirect: isRedirect ? item.path : null,
   };
-
   try {
     insert.run(obj);
   } catch (e) {
@@ -103,50 +81,25 @@ xmlStream.on("tag:page", (page: RawPage) => {
     throw e;
   }
   lastTime = progress(i, "Inserted items: ", lastTime);
-});
+}
 
-xmlStream.on("end", async () => {
-  console.log();
-  console.log(`Inserted ${i} items`);
-  console.log("Committing transaction...");
-  db.run("COMMIT;");
-  console.log("Committing transaction...done");
+console.log();
+console.log(`Inserted ${i} items`);
+console.log("Committing transaction...");
+db.exec("COMMIT;");
+console.log("Committing transaction...done");
 
-  console.log("Inserting data from HTML dump...");
+console.log("Inserting data from HTML dump...");
 
-  const update = db.prepare(`UPDATE pages
-SET text = $text,
-    categories = $categories
-WHERE id = $id`);
-  let j = 0;
-  let lastTime: { time: Date; i: number; diff: number } | undefined;
-  db.run("BEGIN TRANSACTION;");
-  // @ts-ignore what the fuck are you on about a ReadableStream not being for-awaitable
-  for await (const obj of htmlStream) {
-    j++;
-    const html = obj.article_body.html as string;
-    update.run({
-      $text: html.replace(/.*<\/head>/s, ""),
-      $id: obj.identifier,
-      $categories: JSON.stringify(obj.categories?.map((x: any) => x.name)),
-    });
-    lastTime = progress(j, "Inserted items: ", lastTime);
-  }
-  console.log();
-  console.log("Committing transaction...");
-  db.run("COMMIT;");
-  console.log("Committing transaction...done");
-
-  //   console.log("Compressing DB...");
-  //   db.loadExtension(
-  //     "./sqlite_zstd-v0.3.2-x86_64-unknown-linux-gnu/libsqlite_zstd.so",
-  //   );
-  //   db.run(`
-  // select zstd_enable_transparent('{"table": "pages",
-  //     "column": "text", "compression_level": 19,
-  //     "dict_chooser": "''i.'' || (id / 1000000)"}');
-  // select zstd_incremental_maintenance(null, 1);
-  // vacuum;
-  // `);
-  //   console.log("Compressing DB...done");
-});
+//   console.log("Compressing DB...");
+//   db.loadExtension(
+//     "./sqlite_zstd-v0.3.2-x86_64-unknown-linux-gnu/libsqlite_zstd.so",
+//   );
+//   db.run(`
+// select zstd_enable_transparent('{"table": "pages",
+//     "column": "text", "compression_level": 19,
+//     "dict_chooser": "''i.'' || (id / 1000000)"}');
+// select zstd_incremental_maintenance(null, 1);
+// vacuum;
+// `);
+//   console.log("Compressing DB...done");

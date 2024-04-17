b1126e84ba6dbb2413502f067fc18cfbb9b3159b
Kisaragi Hiu <mail@kisaragi-hiu.com>
2024-04-18 05:25:27 +0900

    deal with redlinks

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,6 @@
   import "../app.css";
 </script>
 
-<slot />
+<main class="m-4">
+  <slot />
+</main>
--- src/routes/entry/[title]/+page.svelte
+++ src/routes/entry/[title]/+page.svelte
@@ -1,5 +1,20 @@
 <script lang="ts">
   export let data;
+
+  import { onMount } from "svelte";
+
+  // Wiktionary redlinks encourage readers to create them. Here, showing that
+  // would not be helpful, so we replace them with something else.
+  onMount(() => {
+    for (const aElem of document.querySelectorAll('a[href*="redlink=1"]')) {
+      const parent = aElem.parentNode;
+      if (parent) {
+        const newElem = document.createElement("b");
+        newElem.textContent = aElem.textContent;
+        parent.replaceChild(newElem, aElem);
+      }
+    }
+  });
 </script>
 
 <div class="prose">
